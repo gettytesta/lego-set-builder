8667e8441eaad82603238e1c315e987f146ccadf
Getty Lee Testa <gettytesta@Gettys-MacBook-Pro.local>
2024-01-31 13:52:57 -0500

    Added backend functionality to remove sets.

--- backend/builder.py
+++ backend/builder.py
@@ -23,11 +23,13 @@ sets = [4774,8061,8075,9461,9462,9463,2258,2260,2505,2516,2519,9441,8960,8961,
 4730,4731,3817,7595,5978,7306,3409,7739,6584,6462,6441,5982,8154,8496]
 
 
+# Get all pieces in a set by the set ID
 def get_set_elements(set_num):
     parts = rebrick.lego.get_set_elements(set_num)
     parsedParts = json.loads(parts.read())['results']
     return parsedParts
 
+# Search through the user's list of sets, and determine if a given piece is needed.
 def search_piece(part_num, color):
     for set in sets:
         currentSet = LegoSets[str(set)]
@@ -40,13 +42,14 @@ def search_piece(part_num, color):
                         return 1
                     return 0
 
+# Add piece to db if found
 def found_piece(id, set):
     currentSet = LegoSets[str(set)]
     for piece in currentSet.find():
         if id == piece['_id']:
             currentSet.update_one({'_id': id}, {'$inc': {'obtained_pieces': 1}})
 
-
+# Add a set into the db
 def add_set(set):
     setCluster = LegoSets[str(set)]
     parts = rebrick.lego.get_set_elements(set)
@@ -55,20 +58,25 @@ def add_set(set):
         #setCluster.update_one({'_id': part['id']}, {'$set': {'_id': part['part']['part_num']}})
         setCluster.insert_one({'_id': part['id'], 'part_num': part['part']['part_num'], 'name': part['part']['name'],'color': part['color']['name']
                                 , 'quantity': part['quantity'], 'obtained_pieces': 0})
+        
+# Remove a set in the db
+def remove_set(set):
+    setCluster = LegoSets[str(set)]
+    setCluster.drop()
 
+# Make the setlist from a defined list of user sets
 def make_setlist():
     setlist = LegoSets["setlist"]
     for set in sets:
         setinfo = rebrick.lego.get_set(set)
         parsedSet = json.loads(setinfo.read())
-        #setlist.insert_one({'_id': set, 'num_parts': parsedSet['num_parts'], 'collected_pieces': 0})
-        setlist.update_one({'_id': set}, {'$set': {'set_name': parsedSet['name']}})
+        setlist.insert_one({'_id': set, 'num_parts': parsedSet['num_parts'], 'collected_pieces': 0})
+        #setlist.update_one({'_id': set}, {'$set': {'set_name': parsedSet['name']}})
 
+# Return the list of sets in the db
 def get_setlist():
     newSetList = LegoSets["setlist"].find({})
     retSetList = []
     for set in newSetList:
         retSetList.append({'_id': set['_id'], 'num_parts': set['num_parts'], 'collected_pieces': set['collected_pieces']})
     return retSetList
-
-make_setlist()
